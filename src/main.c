e2531b0b5ece17253cae7c4b7b9977e145bbcc75
Le Non Dupe <lenondupe@gmail.com>
2017-10-09 09:19:27 +0200

    private key length?

--- src/main.c
+++ src/main.c
@@ -663,7 +663,8 @@ unsigned short io_exchange_al(unsigned char channel, unsigned short tx_len) {
 uint32_t set_result_get_publicKey() {
     uint32_t tx = 0;
 
-    os_memmove(G_io_apdu_buffer, tmpCtx.publicKeyContext.publicKey, 56);
+    G_io_apdu_buffer[tx++] = 56;
+    os_memmove(G_io_apdu_buffer+tx, tmpCtx.publicKeyContext.publicKey.W, 56);
     tx += 56;
 
     if (tmpCtx.publicKeyContext.getChaincode) {
@@ -677,15 +678,15 @@ uint32_t set_result_get_publicKey() {
 void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer,
                         uint16_t dataLength, volatile unsigned int *flags,
                         volatile unsigned int *tx) {
+    UNUSED(p1)
     UNUSED(dataLength);
-    uint8_t privateKeyData[56];
+    UNUSED(flags);
+    uint8_t privateKeyData[32];
     uint32_t bip32Path[MAX_BIP32_PATH];
     uint32_t i;
     uint8_t bip32PathLength = *(dataBuffer++);
     cx_ecfp_private_key_t privateKey;
     uint8_t p2Chain = p2 & 0x3F;
-    cx_curve_t curve;
-    uint8_t addressLength;
 
     if ((bip32PathLength < 0x01) || (bip32PathLength > MAX_BIP32_PATH)) {
         PRINTF("Invalid path\n");
@@ -694,7 +695,6 @@ void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer,
     if ((p2Chain != P2_CHAINCODE) && (p2Chain != P2_NO_CHAINCODE)) {
         THROW(0x6B00);
     }
-    curve = CX_CURVE_Ed25519;
 
     for (i = 0; i < bip32PathLength; i++) {
         bip32Path[i] = (dataBuffer[0] << 24) | (dataBuffer[1] << 16) |
@@ -702,12 +702,12 @@ void handleGetPublicKey(uint8_t p1, uint8_t p2, uint8_t *dataBuffer,
         dataBuffer += 4;
     }
     tmpCtx.publicKeyContext.getChaincode = (p2Chain == P2_CHAINCODE);
-    os_perso_derive_node_bip32(curve, bip32Path, bip32PathLength, privateKeyData,
+    os_perso_derive_node_bip32(CX_CURVE_Ed25519, bip32Path, bip32PathLength, privateKeyData,
                                (tmpCtx.publicKeyContext.getChaincode
                                     ? tmpCtx.publicKeyContext.chainCode
                                     : NULL));
-    cx_ecfp_init_private_key(curve, privateKeyData, 56, &privateKey);
-    cx_ecfp_generate_pair(curve, &tmpCtx.publicKeyContext.publicKey, &privateKey, 1);
+    cx_ecfp_init_private_key(CX_CURVE_Ed25519, privateKeyData, 32, &privateKey);
+    cx_ecfp_generate_pair(CX_CURVE_Ed25519, &tmpCtx.publicKeyContext.publicKey, &privateKey, 1);
     os_memset(&privateKey, 0, sizeof(privateKey));
     os_memset(privateKeyData, 0, sizeof(privateKeyData));
 
