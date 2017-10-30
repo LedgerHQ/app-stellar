ec67a633ade1b1aa1a72aa074f3bf2f4d8ff3f27
Le Non Dupe <lenondupe@gmail.com>
2017-10-30 10:04:55 +0100

    some tests for print_amount utility method

--- test/utilstest.c
+++ test/utilstest.c
@@ -17,14 +17,42 @@
  *  limitations under the License.
  ********************************************************************************/
 #include <stdio.h>
+#include <string.h>
 #include "stlr_utils.h"
 
-int main(int argc, char *argv[]) {
-
+void test_print_amount(uint64_t amount, char *expected) {
+    char *asset = "XLM";
     char printed[24];
-    print_amount(0x45d964b800, printed, 22);
-    printf("%s", printed);
-    return 0;
+    print_amount(amount, asset, printed, 22);
+
+    if (strcmp(printed, expected) != 0) {
+        printf("Test failed. Expected: %s; Actual: %s\n", expected, printed);
+    }
 
 }
 
+int main(int argc, char *argv[]) {
+
+    uint64_t amount = 1;
+    char *expected = "0.0000001 XLM";
+    test_print_amount(amount, expected);
+
+    amount = 10000000;
+    expected = "1 XLM";
+    test_print_amount(amount, expected);
+
+    amount = 100000000000001;
+    expected = "10000000.0000001 XLM";
+    test_print_amount(amount, expected);
+
+    amount = 100000001;
+    expected = "10.0000001 XLM";
+    test_print_amount(amount, expected);
+
+    amount = 100000001000000;
+    expected = "10000000.1 XLM";
+    test_print_amount(amount, expected);
+
+    return 0;
+
+}
