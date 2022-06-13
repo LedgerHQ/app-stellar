3da5353eabd8af7fa848c4b4ed32a24efb971472
Jorge Martins <jorge.martins@ledger.fr>
2022-06-13 10:18:07 +0200

    fix build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,11 +4,6 @@
 
 pushd fuzz
 ./build.sh
-#pwd
-#ls
-#ls ./cmake-build-fuzz
-#ldd ./cmake-build-fuzz/fuzz_tx
-#ls -lah /lib/x86_64-linux-gnu/libbsd.so.0
 mv ./cmake-build-fuzz/fuzz_tx $OUT/app-stellar-fuzz-tx
 popd
 
