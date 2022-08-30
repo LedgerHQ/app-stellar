fbe0eed8ebc99980df1db59c9fefc2d90ea4ccf3
Jun Luo <4catcode@gmail.com>
2022-08-30 08:07:58 +0800

    deps: bump @zondax/zemu to 0.33.1

--- tests_zemu/tests/zemu.ts
+++ tests_zemu/tests/zemu.ts
@@ -1,52 +1,33 @@
 import Zemu from "@zondax/zemu";
 import axios from "axios";
 
-function getRandomInt(min: number, max: number): number {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 export default class StellarZemu extends Zemu {
-  private stellarSpeculosApiPort?: number;
-
-  constructor(
-    elfPath: string,
-    libElfs: { [key: string]: string } = {},
-    host: string = "127.0.0.1",
-    desiredTransportPort?: number
-  ) {
-    const desiredSpeculosApiPort = getRandomInt(3000, 65535);  // TODO: Check if it has been used
-    super(elfPath, libElfs, host, desiredTransportPort, desiredSpeculosApiPort);
-    this.stellarSpeculosApiPort = desiredSpeculosApiPort;
-  }
-
   async click(
     endpoint: string,
     filename?: string,
     waitForScreenUpdate?: boolean,
     waitBeforeSnapshot?: number
   ) {
-    let previousScreen;
+    let previousScreen
     if (waitForScreenUpdate) {
-      previousScreen = await this.snapshot();
+      previousScreen = await this.snapshot()
     }
-    const bothClickUrl =
-      "http://localhost:" + this.stellarSpeculosApiPort?.toString() + endpoint;
-    const payload = { action: "press-and-release" };
-    await axios.post(bothClickUrl, payload);
-    this.log(`Click ${endpoint} -> ${filename}`);
+    const bothClickUrl = 'http://localhost:' + this.speculosApiPort?.toString() + endpoint
+    const payload = { action: 'press-and-release' }
+    await axios.post(bothClickUrl, payload)
+    this.log(`Click ${endpoint} -> ${filename}`)
 
     // Wait and poll Speculos until the application screen gets updated
     if (waitForScreenUpdate) {
-      let watchdog = 5000;
-      let currentScreen = await this.snapshot();
+      let watchdog = 5000
+      let currentScreen = await this.snapshot()
       while (currentScreen.data.equals(previousScreen.data)) {
-        this.log("sleep");
-        await Zemu.delay(100);
-        watchdog -= 100;
-        if (watchdog <= 0) throw "Timeout waiting for screen update";
-        currentScreen = await this.snapshot();
+        this.log('sleep')
+        Zemu.delay(100)
+        watchdog -= 100
+        if (watchdog <= 0) throw 'Timeout waiting for screen update'
+        currentScreen = await this.snapshot()
       }
     } else {
       // A minimum delay is required
@@ -62,6 +43,6 @@ export default class StellarZemu extends Zemu {
   }
 
   async clickBoth(filename?: string, waitForScreenUpdate = true, waitBeforeSnapshot = 1000) {
-    return this.click('/button/both', filename, waitForScreenUpdate, waitBeforeSnapshot);
+    return this.click('/button/both', filename, waitForScreenUpdate, waitBeforeSnapshot)
   }
 }
